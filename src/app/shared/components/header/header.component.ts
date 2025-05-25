45d2f02f81629e511be829ba5ee503a0e5d7b84c
Jose Villalta <josevillalta@vimasistem.com>
2025-05-25 13:29:01 -0500

    feat: Usuario logeado header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { LocalStorageService, StorageKeys } from '../../services/local-storage/local-storage.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+	session: any | null = null;
+
+	constructor(
+		private _storageService: LocalStorageService
+	) {
+		this.session = this._storageService.getStorage(StorageKeys.SESSION)?.perfilUsuario;
+	}
 
 }
